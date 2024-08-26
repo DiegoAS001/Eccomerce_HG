12b9928d48345af9cfad6d55a13b66f86ebadc0f
Diego A. dos Santos <diegoaugusto001@gmail.com>
2024-08-26 11:07:56 -0300

    Alteração na clase Produto

--- src/main/java/edu/ifsp/modelo/Produto.java
+++ src/main/java/edu/ifsp/modelo/Produto.java
@@ -1,23 +1,39 @@
 package edu.ifsp.modelo;
 
 public class Produto extends Entity {
-	private String descricao;
+	private String nome;
+	private String tipoMidia;
 	private double preco;
-
-	public String getDescricao() {
-		return descricao;
+	private String categoria;
+	private String plataforma;
+	public String getNome() {
+		return nome;
 	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-
+	public String getTipoMidia() {
+		return tipoMidia;
+	}
+	public void setTipoMidia(String tipoMidia) {
+		this.tipoMidia = tipoMidia;
+	}
 	public double getPreco() {
 		return preco;
 	}
-
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
-
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getPlataforma() {
+		return plataforma;
+	}
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
 }
