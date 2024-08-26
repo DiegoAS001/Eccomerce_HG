c341ce7df396c4721ce6d108ed08dbe7df31505e
Diego A. dos Santos <diegoaugusto001@gmail.com>
2024-08-26 13:23:22 -0300

    Criando os css's cadastro.css e login.css

--- src/main/java/resources/data.sql
+++ src/main/java/resources/data.sql
@@ -4,11 +4,18 @@ INSERT INTO produto (nome, tipoMidia, preco, categoria, plataforma) VALUES
 ('Red Dead Redemption 2', 'D', 49.99, 'Aventura', 'Console'),
 ('Cyberpunk 2077', 'D', 39.99, 'RPG', 'PC'),
 ('Gran Turismo 7', 'D', 69.99, 'Corrida', 'Console'),
+('The Last of Us Part II', 'F', 59.99, 'Ação', 'Console'),
+('Minecraft', 'D', 29.99, 'Aventura', 'PC'),
+('Red Dead Redemption 2', 'D', 49.99, 'Aventura', 'Console'),
+('Cyberpunk 2077', 'D', 39.99, 'RPG', 'PC'),
+('Gran Turismo 7', 'F', 69.99, 'Corrida', 'Console'),
 ('Hades', 'D', 24.99, 'Roguelike', 'PC'),
 ('God of War', 'D', 39.99, 'Ação', 'Console'),
 ('Assassin s Creed Valhalla', 'D', 59.99, 'Ação', 'PC'),
 ('FIFA 23', 'D', 49.99, 'Esporte', 'Console'),
 ('The Witcher 3: Wild Hunt', 'D', 29.99, 'RPG', 'PC');
+('The Witcher 3: Wild Hunt', 'F', 29.99, 'RPG', 'PC');
+
 
   
 INSERT INTO usuario (username, password, salt) VALUES ('das', '1db6b248784f7866500bd34571f2a60fbd3b743e', 'c3ec5fcc-4148-4b2a-86fd-b6b4c9fe928c');
