0d3a544a1cfb13c4ec3af0b8e5f4aa2a11c2127e
Diego A. dos Santos <diegoaugusto001@gmail>
2024-08-28 12:21:02 -0300

    Criação do Servlet Fim.java e do Hmtl fim.html

--- src/main/java/edu/ifsp/web/Fim.java
+++ src/main/java/edu/ifsp/web/Fim.java
@@ -1,4 +1,4 @@
-package edu.ifsp.web.carrinho;
+package edu.ifsp.web;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -10,13 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 import edu.ifsp.web.templates.Template;
 
 
-@WebServlet("/CarrinhoFinalizar")
-public class CarrinhoFinalizar extends HttpServlet {
+@WebServlet("/fim")
+public class Fim extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-   
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Template.render("carrinho/finalizar", request, response);
+		Template.render("carrinho/fim", request, response);
 	}
+
+	
 
 }
